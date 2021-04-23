3b6a918b68b572b17a59c3c2cb42c3b621e816e3
LAMcGuinness <laumcguinness@gmail.com>
2021-04-23 20:49:29 +0000

    updated testing section on ReadMe

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -10,25 +10,24 @@ const userName = document.getElementById('fullname')
 contactForm.addEventListener('submit', function (event) {
         event.preventDefault();
         btn.value = 'Sending'
-}
 
 
 //send form details
-emailjs.sendForm("service_18idldn","template_35epc3x", "#myForm");
-then(() => {
-    btn.value = 'Submit';
-    setTimeout(() => { btn.disabled = true; }, 1000);
-    alert(`Sent`);
-    contactForm.reset();
-    console.log('SUCCESS!', response.status, response.text);
-}, function(error)  {
-    btn.value = 'Submit';
-    btn.disabled = true;
-    alert(alert(JSON.stringify(err)`Opps. Please try again`));
-    console.log('FAILED...', error);
-});
+    emailjs.sendForm("service_18idldn","template_35epc3x", "#myForm");
+        then(() => {
+            btn.value = 'Submit';
+            setTimeout(() => { btn.disabled = true; }, 1000);
+            alert(`Sent`);
+            contactForm.reset();
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error)  {
+            btn.value = 'Submit';
+            btn.disabled = true;
+            alert(alert(JSON.stringify(err)`Opps. Please try again`));
+            console.log('FAILED...', error);
+        });
 //stop
 return false;
 
-};
+});
 
