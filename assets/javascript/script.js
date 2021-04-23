457b1e5f28bfb38af55d4b7738e6ab01a24caf82
LAMcGuinness <laumcguinness@gmail.com>
2021-04-23 09:51:55 +0000

    added JS to make search bar responsive

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,52 +1,30 @@
 // api used https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 
-function getCocktail(){
-  // code from google devoloper
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-     .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+const cocktailList = document.getElementById('cocktailList');
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-        displayCocktail(data);
-      });
+const loadCocktails = async () => {
+    try {
+        const res = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+        let cocktailByIngredient = await res.json();
+        displayCocktail(cocktailByIngredient);
+    } catch (err) {
+        console.error(err);
     }
-)
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-}
-
-getCocktail();
-
-function displayCocktail(cocktail){
-  let cocktailSection = document.querySelector('#cocktail-section');
-  
-  let cocktailName = document.createElement('h2');
-  cocktailName.innerHTML = cocktail.drinks[0].strDrink;
-
-  cocktailSection.appendChild(cocktailName);
-
-  let img = document.createElement('img');
-  img.setAttribute('id', 'cocktailImage');
-  img.src = cocktail.drinks[0].strDrinkThumb;
-
-  cocktailSection.appendChild(img);
-  // maybe cards/bootstrap better?
-
-  // create loop for ingredients and measures 
-
-
-  //for(let i=1; 1<15; i++){
-    //let ingredient = document.createElement('list')
-   // ingredient.innerHTML = cocktail.drinks[0][`strMeasure${i}`] + ':' + cocktail.drinks[0][`strIngredient${i}`];
-
-    //cocktailSection.appendChild(ingredient);
- // }
 };
+
+const displayCocktail = (cocktails) => {
+    const htmlString = cocktails
+    .map((cocktails) => {
+        return `
+        <li class="cocktails">
+            <h2>${cocktails.drinks[0].strDrink}</h2>
+            <p>Method: ${cocktails.drinks[0].strInstructions}</p>
+            <img src="${cocktail.drinks[0].strDrinkThumb}"></img>
+        </li>
+        `;
+    })
+    .join ('');
+    cocktailList.innerHTML = htmlString;
+  };
+
+  loadCocktails();
